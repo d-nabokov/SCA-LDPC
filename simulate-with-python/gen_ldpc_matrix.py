4a13b83b851db31f100cd0cbdef373de3c48802d
Denis Nabokov <denis.nabokov@eit.lth.se>
2025-05-08 11:54:53 +0200

    sort ldpc matrix

--- simulate-with-python/gen_ldpc_matrix.py
+++ simulate-with-python/gen_ldpc_matrix.py
@@ -11,6 +11,8 @@ n = int(argv[1])
 k = int(argv[2])
 row_weight = int(argv[3])
 checks = generate_regular_ldpc_as_tanner(n, k, row_weight)
+# sort the pairs in a way that checks concentrated in the left part are first
+checks = sorted(checks, key=lambda x: x[::-1])
 
 with open(argv[4], "wt") as f:
     for idxs in checks:
